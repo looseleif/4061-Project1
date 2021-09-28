6edc10a7a308062f7d11723d6a4168dbf4dd8293
Chase <chase.anderson.000@gmail.com>
2021-09-27 21:53:19 -0500

    Update test.c

--- sourceCode/test.c
+++ sourceCode/test.c
@@ -4,39 +4,45 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-int main(){
+int main() {
 
-  int i = 0;
-
-  pid_t pid;
-  
-  //for(i;i<5;i++){
-  
-  pid = fork();
-
-  //}
-
-  if(pid < 0){
-    printf("error occured");
-    return 1;
-  } else if(pid == 0){
-
-    // child process
     int i = 0;
-    printf("Im the original child with pid number: %d\n\n",getpid());
-    execlp("/home/looseleif/4061-Project1/outputInput", "/home/looseleif/4061-Project1/outputInput", NULL); // new program
-    
-  } else {
 
-    while(wait(NULL)>0);
-    
-    printf("Child process finished\n");
-    printf("I am parent process with pid: %d\n\n", getpid());
-  }
+    pid_t pid = 1;
 
-  return 0;
+    for (i; i < 100; i++) {
+
+        if (pid > 0) {
+
+            pid = fork();
+
+        }
+
+    }
+
+    if (pid < 0) {
+        printf("error occured");
+        return 1;
+    }
+    else if (pid == 0) {
+
+        // child process
+        int i = 0;
+        printf("Im the original child with pid number: %d\n\n", getpid());
+        execl("./outputInput", "./outputInput", "222", NULL); // new program
+
+    }
+    else {
+
+        while (wait(NULL) > 0);
+
+        printf("Child process finished\n");
+        printf("I am parent process with pid: %d\n\n", getpid());
+    }
+
+    return 0;
 }
 
 
 
-  
+
